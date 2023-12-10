837fcac96a071d402238bec76870ff53e4d473ac
ReneBCurcio <renecbastos@outlook.com>
2023-12-10 10:09:56 -0300

    Adição do mapa e filtro Slider para zoom interativo

--- Dashboard_Analysis.py
+++ Dashboard_Analysis.py
@@ -7,9 +7,12 @@ Cidade = st.sidebar.selectbox("Cidades",df["neighbourhood_group"].value_counts()
 df_Cidade = df[df["neighbourhood_group"] == Cidade]
 Bairro = st.sidebar.selectbox("Bairros",df_Cidade["neighbourhood"].value_counts().index)
 df_Bairro = df_Cidade[df_Cidade["neighbourhood"] == Bairro]
+zoom = st.sidebar.slider(label= "Zoom do Mapa",
+                  min_value=1,
+                  max_value=20)
 st.map(df_Bairro,
     latitude='latitude',
     longitude='longitude',
     size = 0.2,
-    zoom = 12
+    zoom = zoom
 )
